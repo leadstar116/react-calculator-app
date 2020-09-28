50a8aba34ab31d3d452c081f4f7eed607b11f9bc
leadstar116 <supremedev116@gmail.com>
2020-09-28 19:27:31 +1000

    add calculate reducer

---
+++ src/_reducers/calculate.reducer.ts
@@ -0,0 +1,28 @@
+import { CALCULATE_RESET, CALCULATE_UPDATE } from "../_actions/calculate.actions";
+import { CalculatorState } from "../_constants/calculate.interface"
+
+const calculatorState = {
+  next: "",
+  total: "",
+  operation: ""
+} as CalculatorState;
+
+const calculateReducer = (state = calculatorState, action: any) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case CALCULATE_RESET:
+      return {
+        next: "",
+        total: "",
+        operation: ""
+      }
+    case CALCULATE_UPDATE:
+      return payload.data
+    default:
+      break;
+  }
+  return state
+}
+
+export default calculateReducer
