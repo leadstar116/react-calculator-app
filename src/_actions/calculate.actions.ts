12ae2a81ed5606afad84a7f6881bb88ad27cfd1d
leadstar116 <supremedev116@gmail.com>
2020-09-28 19:26:56 +1000

    add calculate actions

---
+++ src/_actions/calculate.actions.ts
@@ -0,0 +1,13 @@
+import { CalculatorState } from "../_constants/calculate.interface"
+
+export const CALCULATE_RESET = 'CALCULATE_RESET'
+export const calculateReset = () => ({
+    type: CALCULATE_RESET,
+    payload: {}
+})
+
+export const CALCULATE_UPDATE = 'CALCULATE_UPDATE'
+export const calculateUpdate = (data: CalculatorState) => ({
+    type: CALCULATE_UPDATE,
+    payload: { data }
+})
